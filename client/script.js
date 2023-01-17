9e9b6a2fd1797e9c1fd40dda0831db36ea817970
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-01-17 12:58:30 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
     // fetch data from server
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://askai-wcpr.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
