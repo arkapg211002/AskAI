ac4faae51b1ce0623eb6de091d7d5dd7b64038c0
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-01-20 11:06:36 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
     // fetch data from server
-    const response = await fetch('https://openai-dle3.onrender.com', {
+    const response = await fetch('https://openaitest-sath.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
